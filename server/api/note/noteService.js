37cb0b9b131adce453fa7a187a55601d459a45ce
Victor Presumido <victor.presumido@gmail.com>
2017-09-03 01:14:26 -0300

    fix api querying bug

--- server/api/note/noteService.js
+++ server/api/note/noteService.js
@@ -1,11 +1,12 @@
 const Note = require('../../models/note');
 
 exports.create = function(req, res) {
-  Note.find().byTitle(req.body.title).exec(function (err, note){
+  const noteTitle = req.body.title;
+  Note.findOne({ title: noteTitle }, function (err, note) {
     if(err){
       res.send(err);
-    }else if (note.length > 0) {
-      return res.status(409).json({ success: false, data: {message: 'Woops, the note '+note[0].title+' already exists.'}});
+    }else if(note != null){
+      return res.status(409).json({ success: false, data: {message: 'Woops, the note '+noteTitle+' already exists.'}});
     }else {
       var newNote = Note({title: req.body.title, text: req.body.text });
       newNote.save(function(err){
@@ -21,9 +22,9 @@ exports.create = function(req, res) {
 exports.show = function(req, res) {
   const noteTitle = req.params.title;
 
-  Note.find().byTitle(noteTitle).exec(function (err, note){
-    if(note.length > 0){
-      res.json(note[0]);
+  Note.findOne({ title: noteTitle }, function (err, note) {
+    if(note != null){
+      res.json(note);
     }else {
       res.status(404).json({ success: false, data: {message: 'This note doesnt exist.'} });
     }
